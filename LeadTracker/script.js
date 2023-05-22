4a1f1c940f1ed594f6c65d7a4f105e51432e04a0
Dipika <123745188+Agnes012@users.noreply.github.com>
2023-05-22 10:01:53 +0545

    Add files via upload

---
+++ LeadTracker/script.js
@@ -0,0 +1,48 @@
+
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const delBtn = document.getElementById ("del-btn")
+const tabBtn = document.getElementById ("tab-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+if (leadsFromLocalStorage) {
+   myLeads = leadsFromLocalStorage
+  render(myLeads)
+}
+
+function render(Leads) {
+    let listItems = ""
+    for (let i = 0; i < Leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${Leads[i]}'>
+                    ${Leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+delBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render( myLeads )
+})
+
+tabBtn.addEventListener("dblclick" , function() {
+   chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+  })
+
+})
